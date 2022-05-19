4a889aea243f984a87c4d1f14b7f4ccc303caf66
saukiprime <56571071+saukiprime@users.noreply.github.com>
2022-05-19 21:00:58 +0800

    初步完成Client模块

--- client/client.go
+++ client/client.go
@@ -1,110 +1,148 @@
 package client
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"strings"
 )
 
 func RunClient() {
-	fmt.Print("TonyDB initiating......")
-	var c Cache
-	c.initCache()
-	line := ""
+	fmt.Println("AlienDB初始化中......")
+	//别管了
+	//var c Cache
+	//c.initCache()
+
+	//与主节点建立连接
+	connMaster := connectToMaster()
 	for {
-		var sql string
-		fmt.Println("新消息>>>请输入你想执行的SQL语句：")
+		var sql, line string
+		fmt.Println(">>>请输入你想执行的SQL语句：")
+		//逐词读取SQL语句
 		for len(line) == 0 || line[len(line)-1] != ';' {
-			fmt.Scanln(&line)
-			if len(line) == 0 {
-				continue
+			_, err := fmt.Scan(&line)
+			if err != nil {
+				panic(err)
 			}
-			sql = sql + line
-			sql += " "
+			sql += line
+			if len(line) != 0 {
+				sql += " "
+			}
 		}
-		line = ""
-		fmt.Println(sql)
 		if strings.Contains(sql, "quit;") {
-			// 退出
 			// 关闭socket 服务器
-			break
-
-		}
-		command := sql
-		sql = ""
-		target := interpreter(command)
-		if _, ok := target["error"]; ok {
-			fmt.Println("新消息>>>输入有误，请重试!")
-		}
-		table := target["name"]
-		var cache string = ""
-		fmt.Println("新消息>>>需要处理的表名是: " + table)
-		/*******
-		待 完 善
-		*/
-		if strings.Compare(target["kind"], "create") == 0 {
-			// 处理建表
-		} else {
-			if strings.Compare(target["Cache"], "true") == 0 {
-				cache = c.getCache(table)
-				if cache == "" {
-					fmt.Println("新消息>>>客户端缓存中不存在该表!")
-				} else {
-					fmt.Println("新消息>>>客户端缓存中存在该表！其对应的服务器是：" + cache)
-				}
-				if cache == "" {
-					// 到masterSocket里面查询
-					// 缺
-				} else {
-					//找到了就直接连接 regionsocketmanager
-					// 缺
-				}
+			err := connMaster.Close()
+			if err != nil {
+				panic(err)
 			}
-		}
-	}
-}
-
-func connectToMaster() {
-	connMaster, err := net.Dial("tcp", "127.0.0.1:8000")
-	if err != nil {
-		panic(err)
-	}
-	defer connMaster.Close()
-	for {
-		msg := ""
-		fmt.Println("发送给Master:", msg)
-		connMaster.Write([]byte(msg))
-
-		data := make([]byte, 255)
-		msg_read, err := connMaster.Read(data)
-		if msg_read == 0 || err != nil {
-			panic(err)
-		}
-		msg_read_str := string(data[0:msg_read])
-		if msg_read_str == "close" {
-			connMaster.Write([]byte("close"))
 			break
 		}
+		//解析操作类型与表名
+		target := interpreter(sql)
+		if _, ok := target["error"]; ok {
+			fmt.Println(">>>输入有误，请重试！")
+		}
+		fmt.Println(">>>需要处理的表名是：" + target["name"])
+		//向主节点发送请求
+		sendToMaster(connMaster, &target)
+		//别管了
+		//if target["kind"] == "create" {
+		//	sendToMaster(connMaster, &target, &c)
+		//} else {
+		//	cache := c.getCache(target["name"])
+		//	if cache == nil {
+		//		fmt.Println(">>>客户端缓存中不存在该表！")
+		//		sendToMaster(connMaster, &target, &c)
+		//		cache = c.getCache(target["name"])
+		//	} else {
+		//		fmt.Println(">>>客户端缓存中存在该表！")
+		//		//fmt.Println(">>>客户端缓存中存在该表！其对应的服务器是：" + cache)
+		//	}
+		//	go connectToRegion(sql, cache)
+		//}
 	}
-
 }
 
-func connectToRegion(sql string, regionIPAddr string) {
-	fmt.Println("新消息>>>与从节点" + regionIPAddr + "建立连接")
-	connRegion, err := net.Dial("tcp", regionIPAddr)
+func sendToMaster(connMaster net.Conn, target *map[string]string) {
+	//map转化为字符串
+	msgStr, _ := json.Marshal(*target)
+	//fmt.Println(msgStr)
+	_, err := connMaster.Write(msgStr)
 	if err != nil {
 		panic(err)
 	}
-	defer connRegion.Close()
-	for {
-		msg := sql
-		fmt.Println("发送给Region:", msg)
-		connRegion.Write([]byte(msg))
-		data := make([]byte, 255)
-		msg_read, err := connRegion.Read(data)
-		if msg_read == 0 || err != nil {
-			panic(err)
+	data := make([]byte, 255)
+	msgRead, err := connMaster.Read(data)
+	if msgRead == 0 || err != nil {
+		panic(err)
+	} else {
+		result := make(map[string]string)
+		//字符串转map
+		err = json.Unmarshal(data, &result)
+		if result["error"] == "" {
+			fmt.Println(">>>操作成功！")
+			if (*target)["kind"] == "select" {
+				fmt.Println(result["data"])
+			}
+			//别管了
+			//if msg["kind"] == "create" {
+			//	fmt.Println(">>>操作成功！")
+			//} else {
+			//	//cache.setCache(msg["name"], result["server"])
+			//	fmt.Println(">>>缓存已更新！")
+			//}
+		} else {
+			fmt.Println(">>>异常：" + result["error"])
 		}
-
 	}
 }
+
+func connectToMaster() net.Conn {
+	var ip, port string
+	fmt.Println(">>>请输入目标服务器IP：")
+	_, err := fmt.Scan(&ip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(">>>请输入目标服务器端口号：")
+	_, err = fmt.Scan(&port)
+	if err != nil {
+		panic(err)
+	}
+	//var connMaster net.Conn
+	connMaster, err := net.Dial("tcp", ip+":"+port)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(">>>连接成功！")
+	}
+	return connMaster
+}
+
+//别管了
+//func connectToRegion(sql string, regionIPAddr []string) {
+//	fmt.Println(">>>与从节点" + regionIPAddr + "建立连接")
+//	connRegion, err := net.Dial("tcp", regionIPAddr)
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer func(connRegion net.Conn) {
+//		err := connRegion.Close()
+//		if err != nil {
+//			panic(err)
+//		}
+//	}(connRegion)
+//	for {
+//		msg := sql
+//		fmt.Println(">>>发送给Region：", msg)
+//		_, err := connRegion.Write([]byte(msg))
+//		if err != nil {
+//			panic(err)
+//		}
+//		data := make([]byte, 255)
+//		msgRead, err := connRegion.Read(data)
+//		if msgRead == 0 || err != nil {
+//			panic(err)
+//		}
+//	}
+//}
